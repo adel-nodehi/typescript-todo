f48f3ca899f9f503989498bc977ba1403c45c770
Adel <adelnmo2003@gmail.com>
2024-09-14 07:34:56 +0330

    Button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,23 @@
+import React, { ReactNode } from "react";
+
+interface ButtonProps {
+  type?: "button" | "submit" | "reset";
+  className?: string;
+  onClick(): void;
+  children: ReactNode;
+}
+
+const Button: React.FC<ButtonProps> = ({
+  type = "button",
+  className = "",
+  onClick,
+  children,
+}) => {
+  return (
+    <button type={type} className={className} onClick={onClick}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
