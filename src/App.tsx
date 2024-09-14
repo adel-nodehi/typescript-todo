088e6e1932bd996536a4c07011aff6b67329d7de
Adel <adelnmo2003@gmail.com>
2024-09-14 07:36:34 +0330

    App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-import React, { useState } from "react";
+import React, { useEffect } from "react";
+import Title from "./components/Title";
+import NewTodo from "./components/NewTodo";
+import TodoList from "./components/TodoList";
 
 const App: React.FC = () => {
-  return <div>Hello</div>;
+  return (
+    <div className="todo-app">
+      <Title>Todo List</Title>
+
+      <NewTodo />
+
+      <TodoList />
+    </div>
+  );
 };
 
 export default App;
