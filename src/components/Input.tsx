51a1212c295c8d4735ef81fbab1097bbcde2aa68
Adel <adelnmo2003@gmail.com>
2024-09-14 08:02:21 +0330

    edit Input component

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { ChangeEvent } from "react";
 
 interface InputProps {
-  className: string;
+  className?: string;
+  value?: string;
+  onChange?(e: React.ChangeEvent<HTMLInputElement>): void;
   placeHolder?: string;
 }
 
-const Input: React.FC<InputProps> = ({ className, placeHolder }) => {
-  return <input type="text" className={className} placeholder={placeHolder} />;
+const Input: React.FC<InputProps> = ({
+  className = "",
+  value = "",
+  onChange = () => {},
+  placeHolder = "",
+}) => {
+  return (
+    <input
+      type="text"
+      className={className}
+      value={value}
+      onChange={onChange}
+      placeholder={placeHolder}
+    />
+  );
 };
 
 export default Input;
