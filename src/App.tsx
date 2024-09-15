a3bc68daba268a0760059631025359489ddc9e9b
Adel <adelnmo2003@gmail.com>
2024-09-15 12:40:02 +0330

    create store and connect in to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import Title from "./components/Title";
 import NewTodo from "./components/NewTodo";
 import TodoList from "./components/TodoList";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 const App: React.FC = () => {
   return (
-    <div className="todo-app">
-      <Title>Todo List</Title>
+    <Provider store={store}>
+      <div className="todo-app">
+        <Title>Todo List</Title>
 
-      <NewTodo />
+        <NewTodo />
 
-      <TodoList />
-    </div>
+        <TodoList />
+      </div>
+    </Provider>
   );
 };
 
