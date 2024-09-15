af53797c96b9a8234a9e311638e27a95da942cec
Adel <adelnmo2003@gmail.com>
2024-09-15 13:09:17 +0330

    update Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,7 +3,7 @@ import React, { ReactNode } from "react";
 interface ButtonProps {
   type?: "button" | "submit" | "reset";
   className?: string;
-  onClick(): void;
+  onClick?(): void;
   children: ReactNode;
 }
 
