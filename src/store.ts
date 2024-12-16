8d6f47f5a23dbd69c27f2555aba30aa2d546daae
Adel Nodehi <adelnmo2003@gmail.com>
2024-12-16 15:22:35 +0330

    feat(localStorage): add localStorage to todo app
    save and retrieve todos from localStorage

--- src/store.ts
+++ src/store.ts
@@ -1,10 +1,20 @@
 import { configureStore } from "@reduxjs/toolkit";
 import todoSlice from "./components/todoSlice";
 
+import { loadTodos, setTodos } from "./utils/localStorage";
+
 export const store = configureStore({
   reducer: {
     todo: todoSlice,
   },
+  preloadedState: {
+    todo: loadTodos(),
+  },
+});
+
+store.subscribe(() => {
+  const state = store.getState().todo;
+  setTodos(state);
 });
 
 export type RootState = ReturnType<typeof store.getState>;
---
+++ src/utils/localStorage.ts
@@ -0,0 +1,11 @@
+import { Todo } from "../types/todo.model";
+
+export const loadTodos = () => {
+  const todos = localStorage.getItem("todo-list");
+  return todos === null ? undefined : JSON.parse(todos);
+};
+
+export const setTodos = (todos: Todo[]) => {
+  console.log(todos);
+  localStorage.setItem("todo-list", JSON.stringify(todos));
+};
